dabf7a7d4d8a120a7c4eab68cf92c6b88ceccfd0
Guy Carmeli <guyca@users.noreply.github.com>
2020-07-29 07:55:11 +0300

    Fab id is mandatory (#6435)
    When updating FAB options with mergeOptions, id must be passed as well.

--- lib/src/interfaces/Options.ts
+++ lib/src/interfaces/Options.ts
@@ -574,11 +574,7 @@ export interface ElementAnimation {
 }
 
 export interface OptionsFab {
-  /**
-   * ID is required when first instantiating the Fab button,
-   * however when updating the existing Fab button, ID is not required.
-   */
-  id?: string;
+  id: string;
   backgroundColor?: Color;
   clickColor?: Color;
   rippleColor?: Color;
