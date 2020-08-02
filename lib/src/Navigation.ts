c5b34a329beb1aacc8650c9b82fd541daf11b0d6
Guy Carmeli <guyc@wix.com>
2020-08-02 12:49:27 +0300

    Fix promise resolve type from any to string

--- lib/src/Navigation.ts
+++ lib/src/Navigation.ts
@@ -162,7 +162,7 @@ export class NavigationRoot {
   /**
    * Reset the app to a new layout
    */
-  public setRoot(layout: LayoutRoot): Promise<any> {
+  public setRoot(layout: LayoutRoot): Promise<string> {
     return this.commands.setRoot(layout);
   }
 
@@ -190,56 +190,59 @@ export class NavigationRoot {
   /**
    * Show a screen as a modal.
    */
-  public showModal<P>(layout: Layout<P>): Promise<any> {
+  public showModal<P>(layout: Layout<P>): Promise<string> {
     return this.commands.showModal(layout);
   }
 
   /**
    * Dismiss a modal by componentId. The dismissed modal can be anywhere in the stack.
    */
-  public dismissModal(componentId: string, mergeOptions?: Options): Promise<any> {
+  public dismissModal(componentId: string, mergeOptions?: Options): Promise<string> {
     return this.commands.dismissModal(componentId, mergeOptions);
   }
 
   /**
    * Dismiss all Modals
    */
-  public dismissAllModals(mergeOptions?: Options): Promise<any> {
+  public dismissAllModals(mergeOptions?: Options): Promise<string> {
     return this.commands.dismissAllModals(mergeOptions);
   }
 
   /**
    * Push a new layout into this screen's navigation stack.
    */
-  public push<P>(componentId: string, layout: Layout<P>): Promise<any> {
+  public push<P>(componentId: string, layout: Layout<P>): Promise<string> {
     return this.commands.push(componentId, layout);
   }
 
   /**
    * Pop a component from the stack, regardless of it's position.
    */
-  public pop(componentId: string, mergeOptions?: Options): Promise<any> {
+  public pop(componentId: string, mergeOptions?: Options): Promise<string> {
     return this.commands.pop(componentId, mergeOptions);
   }
 
   /**
    * Pop the stack to a given component
    */
-  public popTo(componentId: string, mergeOptions?: Options): Promise<any> {
+  public popTo(componentId: string, mergeOptions?: Options): Promise<string> {
     return this.commands.popTo(componentId, mergeOptions);
   }
 
   /**
    * Pop the component's stack to root.
    */
-  public popToRoot(componentId: string, mergeOptions?: Options): Promise<any> {
+  public popToRoot(componentId: string, mergeOptions?: Options): Promise<string> {
     return this.commands.popToRoot(componentId, mergeOptions);
   }
 
   /**
    * Sets new root component to stack.
    */
-  public setStackRoot<P>(componentId: string, layout: Layout<P> | Array<Layout<P>>): Promise<any> {
+  public setStackRoot<P>(
+    componentId: string,
+    layout: Layout<P> | Array<Layout<P>>
+  ): Promise<string> {
     const children: Layout[] = isArray(layout) ? layout : [layout];
     return this.commands.setStackRoot(componentId, children);
   }
@@ -247,14 +250,14 @@ export class NavigationRoot {
   /**
    * Show overlay on top of the entire app
    */
-  public showOverlay<P>(layout: Layout<P>): Promise<any> {
+  public showOverlay<P>(layout: Layout<P>): Promise<string> {
     return this.commands.showOverlay(layout);
   }
 
   /**
    * dismiss overlay by componentId
    */
-  public dismissOverlay(componentId: string): Promise<any> {
+  public dismissOverlay(componentId: string): Promise<string> {
     return this.commands.dismissOverlay(componentId);
   }
 
