5db381690577e37c9a7931331dd55024bf2d4a9d
Ward Abbass <swabbass@gmail.com>
2021-03-02 11:04:11 +0200

    apply & merge options in BottomTabsPresenter

--- lib/ios/BottomTabPresenter.m
+++ lib/ios/BottomTabPresenter.m
@@ -8,8 +8,10 @@
 - (void)applyOptions:(RNNNavigationOptions *)options child:(UIViewController *)child {
     RNNNavigationOptions *withDefault = [options withDefault:self.defaultOptions];
 
-    [child setTabBarItemBadge:[withDefault.bottomTab.badge withDefault:[NSNull null]]];
-    [child setTabBarItemBadgeColor:[withDefault.bottomTab.badgeColor withDefault:nil]];
+    [child setTabBarItemBadge:[withDefault.bottomTab.badge.text withDefault:[NSNull null]]];
+    [child setTabBarItemBadgeColor:[withDefault.bottomTab.badge.backgroundColor withDefault:nil]];
+    [child setTabBarItemBadgeTextColor:[withDefault.bottomTab.badge.textColor
+                                           withDefault:[UIColor whiteColor]]];
 }
 
 - (void)applyOptionsOnWillMoveToParentViewController:(RNNNavigationOptions *)options
@@ -17,8 +19,10 @@
     RNNNavigationOptions *withDefault = [options withDefault:self.defaultOptions];
 
     [self createTabBarItem:child bottomTabOptions:withDefault.bottomTab];
-    [child setTabBarItemBadge:[withDefault.bottomTab.badge withDefault:[NSNull null]]];
-    [child setTabBarItemBadgeColor:[withDefault.bottomTab.badgeColor withDefault:nil]];
+    [child setTabBarItemBadge:[withDefault.bottomTab.badge.text withDefault:[NSNull null]]];
+    [child setTabBarItemBadgeColor:[withDefault.bottomTab.badge.backgroundColor withDefault:nil]];
+    [child setTabBarItemBadgeTextColor:[withDefault.bottomTab.badge.textColor
+                                           withDefault:[UIColor blackColor]]];
 }
 
 - (void)mergeOptions:(RNNNavigationOptions *)mergeOptions
@@ -29,10 +33,13 @@
 
     if (mergeOptions.bottomTab.hasValue)
         [self createTabBarItem:child bottomTabOptions:withDefault.bottomTab];
-    if (mergeOptions.bottomTab.badge.hasValue)
-        [child setTabBarItemBadge:mergeOptions.bottomTab.badge.get];
-    if (mergeOptions.bottomTab.badgeColor.hasValue)
-        [child setTabBarItemBadgeColor:mergeOptions.bottomTab.badgeColor.get];
+    if (mergeOptions.bottomTab.badge.text.hasValue)
+        [child setTabBarItemBadge:mergeOptions.bottomTab.badge.text.get];
+    if (mergeOptions.bottomTab.badge.backgroundColor.hasValue)
+        [child setTabBarItemBadgeColor:mergeOptions.bottomTab.badge.backgroundColor.get];
+    if (mergeOptions.bottomTab.badge.textColor.hasValue)
+        [child setTabBarItemBadgeTextColor:[mergeOptions.bottomTab.badge.textColor
+                                               withDefault:[UIColor blackColor]]];
 }
 
 - (void)createTabBarItem:(UIViewController *)child
