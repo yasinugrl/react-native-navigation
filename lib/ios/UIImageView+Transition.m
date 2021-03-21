5c94f93972582275c248d30a6cdef899bd81b037
Yogev Ben David <yogev132@gmail.com>
2021-03-21 11:00:04 +0200

    Support SET for images with different resizeMode

---
+++ lib/ios/UIImageView+Transition.m
@@ -0,0 +1,31 @@
+#import "UIImageView+Transition.h"
+
+@implementation UIImageView (Transition)
+
+- (CGRect)resolveBounds {
+    switch (self.contentMode) {
+    case UIViewContentModeCenter:
+        return CGRectMake(0, 0, self.image.size.width, self.image.size.height);
+        break;
+    case UIViewContentModeScaleAspectFill: {
+        CGSize imageSize = CGSizeMake(self.image.size.width, self.image.size.height);
+
+        CGFloat widthRatio = imageSize.width / self.superview.bounds.size.width;
+        CGFloat heightRatio = imageSize.height / self.superview.bounds.size.height;
+
+        if (widthRatio > heightRatio) {
+            imageSize = CGSizeMake(imageSize.width / heightRatio, imageSize.height / heightRatio);
+        } else {
+            imageSize = CGSizeMake(imageSize.width / widthRatio, imageSize.height / widthRatio);
+        }
+
+        return CGRectMake(0, 0, imageSize.width, imageSize.height);
+    }
+    default:
+        break;
+    }
+
+    return self.bounds;
+}
+
+@end
