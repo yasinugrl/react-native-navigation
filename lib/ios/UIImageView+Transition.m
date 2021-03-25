db22e2430bd7b1a3fd253011a5c12cbbfe0d3d82
Yogev Ben David <yogev132@gmail.com>
2021-03-25 13:32:51 +0200

    Add contain resizeMode support

--- lib/ios/UIImageView+Transition.m
+++ lib/ios/UIImageView+Transition.m
@@ -21,6 +21,21 @@
 
         return CGRectMake(0, 0, imageSize.width, imageSize.height);
     }
+    case UIViewContentModeScaleAspectFit: {
+        CGSize imageSize = CGSizeMake(self.image.size.width / self.image.scale,
+                                      self.image.size.height / self.image.scale);
+
+        CGFloat widthRatio = imageSize.width / self.superview.bounds.size.width;
+        CGFloat heightRatio = imageSize.height / self.superview.bounds.size.height;
+
+        if (widthRatio > heightRatio) {
+            imageSize = CGSizeMake(imageSize.width / widthRatio, imageSize.height / widthRatio);
+        } else {
+            imageSize = CGSizeMake(imageSize.width / heightRatio, imageSize.height / heightRatio);
+        }
+
+        return CGRectMake(0, 0, imageSize.width, imageSize.height);
+    }
     default:
         break;
     }
